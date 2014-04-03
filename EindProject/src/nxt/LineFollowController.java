0c6257576edc883b1e3cf17ce34dc4b9fbb5b5e5
djrobert383 <djrobert@live.nl>
2014-04-03 14:58:57 +0200

    voor jacobus de 3e

--- EindProject/src/nxt/LineFollowController.java
+++ EindProject/src/nxt/LineFollowController.java
@@ -1,13 +1,16 @@
 package nxt;
 
+import lejos.nxt.LCD;
+import lejos.robotics.navigation.DifferentialPilot;
+
 /**
  * 
  * @author Robert Bezem <robert.bezem@student.hu.nl>
  * @version 0.2
  */
 public class LineFollowController extends Thread implements LightSensorListener {
-	private boolean leftOnRoute;
-	private boolean rightOnRoute;
+	private boolean leftBlack;
+	private boolean rightBlack;
 	private static boolean pause = false;
 	private final int TRESHOLD = 50;
 
@@ -16,16 +19,17 @@ public class LineFollowController extends Thread implements LightSensorListener 
 	public LineFollowController(ColorSensor cs, LightSensor ls) {
 		cs.addListener(this);
 		ls.addListener(this);
+		MotorController.setTravelSpeed(200);
 		this.start();
 	}
 
 	public void run() {
 		while (true) {
 			if (!pause) {
-				if (!leftOnRoute) {
-					MotorController.turnOnPlace(-5);
-				} else if (!rightOnRoute) {
-					MotorController.turnOnPlace(5);
+				if (!leftBlack) {
+					MotorController.turnOnPlace(-1);
+				} else if (!rightBlack) {
+					MotorController.turnOnPlace(1);
 				} else {
 					MotorController.driveForward();
 				}
@@ -37,17 +41,20 @@ public class LineFollowController extends Thread implements LightSensorListener 
 	public void lightSensorChanged(SensorPosition position,
 			UpdatingSensor updatingsensor, float oldValue, float newValue) {
 		if (position == SensorPosition.Left) {
+			LCD.drawString("" + newValue, 0, 0);
 			if (newValue < TRESHOLD) {
-				leftOnRoute = false;
+				leftBlack = true;
 			} else {
-				leftOnRoute = true;
+				leftBlack = false;
 			}
 		}
 		if (position == SensorPosition.Right) {
+			LCD.drawString("" + newValue, 0, 1);
 			if (newValue < TRESHOLD) {
-				rightOnRoute = false;
+
+				rightBlack = true;
 			} else {
-				rightOnRoute = true;
+				rightBlack = false;
 			}
 		}
 	}
