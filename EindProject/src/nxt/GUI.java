4896e98e8496d9a0406a3888e9dc5a58e221f4ec
djrobert383 <djrobert@live.nl>
2014-04-05 18:18:40 +0200

    GUI functions made

--- EindProject/src/nxt/GUI.java
+++ EindProject/src/nxt/GUI.java
@@ -8,31 +8,68 @@ import lejos.nxt.LCD;
 
 public class GUI {
 	private Graphics g;
-	private boolean error = false;
+	private boolean errorPopUp = false;
 
 	public GUI() {
 		g = new Graphics();
 		LCD.setAutoRefresh(false);
 	}
 
-	public void showError(String errorMessage) {
-		error = true;
+	public void showErrorPopUp(String errorMessage) {
+		errorPopUp = true;
 		Font font = Font.getSmallFont();
-		int imageXPosition = (LCD.SCREEN_WIDTH / 2) - Icons.error.getWidth();
+		int imageXPosition = (g.getHeight() / 2) - (Icons.error.getWidth() / 2);
 		int stringYPosition = Icons.error.getHeight() + 2;
 		int stringXPosition = errorMessage.length() / 2;
 
+		int borderHeight = Icons.error.getHeight() + font.getHeight();
+		int borderWidth;
+		if (errorMessage.length() < Icons.error.getWidth()) {
+			borderWidth = Icons.error.getWidth();
+		} else {
+			borderWidth = errorMessage.length();
+		}
+
+		int borderLeftPosition = (g.getWidth() / 2) - borderWidth;
+		int borderUpPosition = imageXPosition - 5;
+
 		g.setFont(font);
+		g.clear();
+		g.drawRect(borderLeftPosition, borderUpPosition, borderWidth,
+				borderHeight);
 		g.drawImage(Icons.error.getIcon(), imageXPosition, 1, 0);
 		g.drawString(errorMessage, stringXPosition, stringYPosition, 1);
 
 	}
 
 	public void sensorAlright(SensorPosition position) {
-		if (!error) {
+		if (!errorPopUp) {
 			Font font = Font.getSmallFont();
 			g.setFont(font);
 			Image icon = Icons.ok.getIcon();
+			int imageYposition = font.getHeight() + 1;
+			if (position == SensorPosition.Left) {
+				String message = "Left sensor";
+				
+				g.drawString("Left sensor", 0, 0, 1);
+				g.drawImage(icon, 0, imageYposition, 1);
+
+			} else {
+				String message = "Right Sensor";
+				int stringXPosition = 100 - message.length();
+				int imageXPosition = 100 - icon.getWidth();
+
+				g.drawString(message, stringXPosition, 0, 1);
+				g.drawImage(icon, imageXPosition, imageXPosition, 1);
+			}
+		}
+	}
+
+	public void lightensorError(SensorPosition position) {
+		if (!errorPopUp) {
+			Font font = Font.getSmallFont();
+			g.setFont(font);
+			Image icon = Icons.error.getIcon();
 			int imageYposition = font.getHeight() + 1;
 			if (position == SensorPosition.Left) {
 				String message = "Left sensor";
@@ -51,18 +88,8 @@ public class GUI {
 		}
 	}
 
-	public void sensorError(SensorPosition position) {
-		if (!error) {
-			if (position == SensorPosition.Left) {
-
-			} else {
-
-			}
-		}
-	}
-
-	public void cancleError() {
-		error = false;
+	public void cancleErrorPopUp() {
+		errorPopUp = false;
 		LCD.clear();
 	}
 }
