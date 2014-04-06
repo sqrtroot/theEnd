c99dbac976f930581d5d0eca13bfecf7a3a4c3d0
pimvanhespen <pimvanhespen@gmail.com>
2014-04-06 19:44:13 +0200

    updated obstruction controller

--- EindProject/src/nxt/ObstructionController.java
+++ EindProject/src/nxt/ObstructionController.java
@@ -1,28 +1,67 @@
 package nxt;
+/**
+ * @author Pim van Hespen <Pimvanhespen@gmail.com>
+ * @version 1.1
+ * @since 01-04-2014
+ * 
+ *        This is class will evade objects
+ * 
+ */
+public class ObstructionController extends Thread implements
+		LightSensorListener, UltraSonicSensorListener {
 
-public class ObstructionController implements LightSensorListener,
-		UltraSonicSensorListener {
+	private int current_distance; // /<
+	private int sensor_value_left;
+	private int sensor_value_right;
 
-	private int current_distance = 255;
-	private int sensor_value_left = 0;
-	private int sensor_value_right = 0;
-
-	public static final int SAFE_DISTANCE = 20;
-	private final int ARC_DEGREES = 180;
-
-	//private final int LIGHT_HIGH = 70; unnecessary
+	private final int SAFE_DISTANCE = 20;
+	private final int ARC_DEGREES = 360;
+	
+	private final int LIGHT_HIGH = 70;
 	private final int LIGHT_LOW = 30;
-	private GUI gui;
+	
+	private final int MEDIAN = 50;
 
-	public ObstructionController(ColorSensor cs, LightSensor ls,
-			UltraSonicSensor us, GUI gui) {
+	private GUI gui;
+	
+	public ObstructionController(ColorSensor cs, LightSensor ls, UltraSonicSensor us, GUI gui) {
+		
 		this.gui = gui;
+
 		cs.addListener(this);
 		ls.addListener(this);
 		us.addListener(this);
+
+		current_distance = 255;
+		sensor_value_left = 0;
+		sensor_value_right = 0;
 	}
 
-	private void evasiveManoeuvre() {
+	/**
+	 * Is called upon by Obstructioncontroller.start. This will run the
+	 * controller in a new Thread. If the robot encounters an object within the
+	 * given safe distance, the loop will call upon the evasiveManeuver method
+	 * to bypass any object.
+	 * 
+	 * @see java.lang.Thread#run()
+	 */
+	public void run() {
+		while (true) {
+			MotorController.driveForward();
+			if (current_distance < SAFE_DISTANCE) {
+				evasiveManeuver();
+			}
+		}
+
+	}
+
+	/**
+	 * This method checks the current distance to the object. Whenever the
+	 * object distance is smaller than the safe distance, this method will
+	 * perform a 360 degree circle around the found object and stops driving the
+	 * circle when the line has been found again.
+	 */
+	private void evasiveManeuver() {
 
 		MotorController.driveForward();
 		boolean noLineFound = true;
@@ -31,15 +70,13 @@ public class ObstructionController implements LightSensorListener,
 			if (current_distance < SAFE_DISTANCE) {
 				LineFollowController.pauseLineFollowing();
 				MotorController.turnOnPlace(-90);
-				MotorController.DriveArc((SAFE_DISTANCE * 10), ARC_DEGREES,
-						true); // RADIUS
-				// ,
-				// ANGLE
+				MotorController.DriveArc((SAFE_DISTANCE * 10), ARC_DEGREES,	true);
+
 
 				while (MotorController.moving()) {
 
-					if (sensor_value_left < LIGHT_LOW
-							|| sensor_value_right < LIGHT_LOW)
+					if (sensor_value_left < MEDIAN
+							|| sensor_value_right < MEDIAN)
 						MotorController.stop();
 
 				}
@@ -51,33 +88,22 @@ public class ObstructionController implements LightSensorListener,
 		}
 
 	}
-
-	/*
-	 * Wats deze?? private void evasiveManoeuvre() {
-	 * 
-	 * MotorController.stop();
-	 * MotorController.driveArc((SAFE_DISTANCE*10),true);
-	 * 
-	 * while(true){ System.out.println(sensor_value_left + " \t" +
-	 * sensor_value_right); if(sensor_value_left < LIGHT_LOW){
-	 * MotorController.stop();
-	 * 
-	 * 
-	 * } else if(sensor_value_right < LIGHT_LOW){ MotorController.stop();
-	 * 
-	 * } }
-	 */
-
 	@Override
 	public void ultraSonicChanged(UpdatingSensor us, float oldValue,
 			float newValue) {
+
 		if (newValue < SAFE_DISTANCE) {
 			gui.showErrorPopUp("Object to close");
-			evasiveManoeuvre();
+			evasiveManeuver();
 		} else {
 			gui.canclePopUp();
 		}
+
+
+		current_distance = (int) newValue;
+
 	}
+
 
 	@Override
 	public void lightSensorChanged(SensorPosition position,
