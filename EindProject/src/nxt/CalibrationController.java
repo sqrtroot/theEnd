3faba9aca82542ffea7a5cb8bc5cda1adafac938
pimvanhespen <pimvanhespen@gmail.com>
2014-04-06 19:46:32 +0200

    CalibrationController out of memory error fixed.

--- EindProject/src/nxt/CalibrationController.java
+++ EindProject/src/nxt/CalibrationController.java
@@ -4,9 +4,8 @@ import java.util.ArrayList;
 import lejos.nxt.ColorSensor.Color;
 
 /**
- * @author Pim van Hespen <pim.vanhespen@hu.nl>
- * @author Robert Bezem <robert.bezem@student.hu.nl>
- * @version 1.0
+ * @author Pim van Hespen <pimvanhespen@gmail.com>
+ * @version 1.2
  * @since 02-04-2014
  * 
  *        This class calibrates both Light and ColorSensor for further usage.
@@ -15,13 +14,6 @@ public class CalibrationController {
 
 	private ColorSensor cs; // /< ColorSensor object.
 	private LightSensor ls; // /< LightSensor object.
-
-	private ArrayList<Integer> lightValues; // /< ArrayList to be filled with
-											// integers from readings of the
-											// LightSensor
-	private ArrayList<Integer> colorValues; // /< ArrayList to be filled with
-											// integers from readings of the
-											// ColorSensor
 
 	private final int CIRCLE = 360; // /< 360, the amount of degrees in a
 									// circle, therefore called CIRCLE
@@ -45,9 +37,8 @@ public class CalibrationController {
 		this.cs = colorSensor;
 		this.ls = lightSensor;
 
-		lightValues = new ArrayList<Integer>();
-		colorValues = new ArrayList<Integer>();
 		gui.showPopUp("Calibrating");
+
 		calibrateAllSensors();
 		gui.canclePopUp();
 		LineFollowController.continueLineFollowing();
@@ -60,8 +51,7 @@ public class CalibrationController {
 	public void calibrateAllSensors() {
 
 		// ensure the flood light color form the ColorSensor is red, then turn
-		// on
-		// the lights from both sensors.
+		// on the lights from both sensors.
 		cs.setFloodlight(Color.RED);
 		cs.setFloodlight(true);
 		ls.setFloodlight(true);
@@ -72,18 +62,29 @@ public class CalibrationController {
 		MotorController.setRotateSpeed(45);
 		MotorController.rotate(CIRCLE, true);
 
+		// integers used to store all the required values for the calibration.
+		int colorLow = 1023, colorHigh = 0, lightLow = 1023, lightHigh = 0;
+		int currentLightValue = 0, currentColorValue = 0;
+		
 		while (MotorController.moving()) {
-			lightValues.add(ls.getNormalizedLightValue());
-			colorValues.add(cs.getRawLightValue());
+			
+			currentLightValue = ls.getNormalizedLightValue();
+			currentColorValue = cs.getRawLightValue();
+			
+			if(currentColorValue  > colorHigh)colorHigh = currentColorValue;
+			else if(currentColorValue < colorLow)colorLow = currentColorValue;
+			if(currentLightValue > lightHigh)lightHigh = currentLightValue;
+			else if(currentLightValue < lightLow)lightLow = currentLightValue;
+			
 		}
 
 		// Set the highest and lowest measured values from the measurements as
 		// new high and low for the sensors to calibrate them and give them
 		// their new range.
-		ls.setHigh(getHighestValue(lightValues));
-		ls.setLow(getLowestValue(lightValues));
-		cs.setHigh(getHighestValue(colorValues));
-		cs.setLow(getLowestValue(colorValues));
+		ls.setHigh(lightHigh);
+		ls.setLow(lightLow);
+		cs.setHigh(colorHigh);
+		cs.setLow(colorLow);
 
 		// Drive another circle until the black line is found.
 		MotorController.rotate(CIRCLE, true);
@@ -93,46 +94,6 @@ public class CalibrationController {
 				MotorController.stop();
 			}
 		}
-	}
-
-	/**
-	 * @param al
-	 *            the given ArrayList to filter the highest value from.
-	 * @return highestValue the highest value found in the given arrayList.
-	 * @see ArrayList
-	 */
-	public int getHighestValue(ArrayList<Integer> al) {
-		// Create an integer to store the highest value, then process all the
-		// measurements stored in the ArrayList and compare them with the
-		// current highest value. If the highest value in the 'highestValue' is
-		// lower than the current highest value. Replace highest value with the
-		// current value.
-		int highestValue = 0;
-		for (int currentValue : al) {
-			if (currentValue > highestValue)
-				highestValue = currentValue;
-		}
-		return highestValue;
-	}
-
-	/**
-	 * @param al
-	 *            the given ArrayList to filter the lowest value from.
-	 * @return lowestValue the lowest value found in the given arrayList.
-	 * @see ArrayList
-	 */
-	public int getLowestValue(ArrayList<Integer> al) {
-		// Create an integer to store the lowest value, then process all the
-		// measurements stored in the ArrayList and compare them with the
-		// current lowest value. If the lowest value in the 'lowestValue' is
-		// lower than the currentValue. Replace the lowest value with the
-		// current value.
-		int lowestValue = 1023;
-		for (int currentValue : al) {
-			if (currentValue < lowestValue)
-				lowestValue = currentValue;
-		}
-		return lowestValue;
 	}
 
 }
