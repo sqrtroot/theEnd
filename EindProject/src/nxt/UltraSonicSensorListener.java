6d1143a09e2a1d22245722ba2d7695b63b0956e6
Ban-aan <Becommenteerd door Ban-aan. Sms ban-aan naar 6464.>
2014-04-07 13:34:02 +0200

    More More More text

--- EindProject/src/nxt/UltraSonicSensorListener.java
+++ EindProject/src/nxt/UltraSonicSensorListener.java
@@ -7,6 +7,17 @@ package nxt;
  * @since 01-04-2014
  */
 public interface UltraSonicSensorListener {
+	/**
+	 * When implemented this method can be called to do something when the measured value
+	 * from the ultrasonicsensor changes 
+	 * 
+	 * @param us
+	 * 			The sensor from which the value is changed
+	 * @param oldValue
+	 * 			The old value from the sensor
+	 * @param newValue
+	 * 			The new value from the sensor
+	 */
 	public void ultraSonicChanged(UpdatingSensor us, float oldValue,
 			float newValue);
 }
--- EindProject/src/nxt/UpdatingSensor.java
+++ EindProject/src/nxt/UpdatingSensor.java
@@ -7,7 +7,14 @@ package nxt;
  * @since 01-04-2014
  */
 public interface UpdatingSensor {
+	/**
+	 * When implemented this method can update the measured values from a sensor 
+	 */
 	public void updateState();
-
+	/**
+	 * Every sensor is from a different type, with this method you can determine the sensortype
+	 * @return Sensortype 
+	 * 					returns the specific sensortype
+	 */
 	public SensorType getSensorType();
 }
