996050ded8fd7d52f00982ff8b817302e9c9e9a5
djrobert383 <djrobert@live.nl>
2014-04-08 11:48:14 +0200

    gui gefixed

--- EindProject/src/gui/GUI.java
+++ EindProject/src/gui/GUI.java
@@ -5,6 +5,7 @@ import javax.microedition.lcdui.Graphics;
 import javax.microedition.lcdui.Image;
 
 import sensors.SensorPosition;
+import lejos.nxt.Button;
 import lejos.nxt.LCD;
 
 /**
@@ -16,13 +17,18 @@ import lejos.nxt.LCD;
 public class GUI {
 	private Graphics g;
 	private boolean popUp = false;
+	private static final int messageBoxTopPadding = 5;
+	private static final int messageBoxLeftPadding = 2;
+	private static final int fontPixalWidth = 6;
+	private static final Font font = Font.getDefaultFont();
+	private static final int borderXPosition = 0;
+	private static final int borderYPosition = 0;
 
 	/**
 	 * the constructor for the gui class
 	 */
 	public GUI() {
 		g = new Graphics();
-		LCD.setAutoRefresh(false);
 	}
 
 	/**
@@ -33,32 +39,28 @@ public class GUI {
 	 */
 	public void showErrorPopUp(String errorMessage) {
 		popUp = true;
-		Font font = Font.getSmallFont();
-		int imageXPosition = (g.getHeight() / 2) - (Icons.error.getWidth() / 2);
 
-		int borderHeight = Icons.error.getHeight() + font.getHeight();
+		int borderHeight = Icons.error.getHeight() + font.getHeight()
+				+ messageBoxTopPadding;
 		int borderWidth;
-		if (errorMessage.length() < Icons.error.getWidth()) {
-			borderWidth = Icons.error.getWidth();
+		if (errorMessage.length() * fontPixalWidth < Icons.error.getWidth()) {
+			borderWidth = Icons.error.getWidth() + messageBoxLeftPadding;
 		} else {
-			borderWidth = errorMessage.length();
+			borderWidth = errorMessage.length() * fontPixalWidth
+					+ messageBoxLeftPadding;
 		}
 
-		int borderLeftPosition = (g.getWidth() / 2) - borderWidth;
-		int borderUpPosition = 5;
+		int imageXPosition = (borderWidth / 2) - (Icons.error.getWidth()/2);
+		int imageYPosition = borderYPosition + messageBoxTopPadding;
 
-		int imageYPosition = borderUpPosition + 1;
-
-		int stringYPosition = imageYPosition + Icons.error.getHeight() + 2;
-		int stringXPosition = (errorMessage.length() / 2) + borderLeftPosition
-				+ 2;
+		int stringYPosition = imageYPosition + Icons.error.getHeight();
+		int stringXPosition = borderXPosition + messageBoxLeftPadding;
 
 		g.setFont(font);
 		g.clear();
-		g.drawRect(borderLeftPosition, borderUpPosition, borderWidth,
-				borderHeight);
+		g.drawRect(borderXPosition, borderYPosition, borderWidth, borderHeight);
 		g.drawImage(Icons.error.getIcon(), imageXPosition, 1, 0);
-		g.drawString(errorMessage, stringXPosition, stringYPosition, 1);
+		g.drawString(errorMessage, stringXPosition, stringYPosition, 0);
 
 	}
 
@@ -70,21 +72,18 @@ public class GUI {
 	 */
 	public void showPopUp(String message) {
 		popUp = true;
-		Font font = Font.getSmallFont();
 
-		int borderHeight = font.getHeight() + 5;
-		int borderWidth = message.length() + 2;
+		int borderHeight = font.getHeight() + messageBoxTopPadding;
+		int borderWidth = message.length() * fontPixalWidth
+				+ messageBoxLeftPadding;
 
-		int borderLeftPosition = (g.getWidth() / 2) - borderWidth;
-		int borderUpPosition = 5;
-		int stringYPosition = borderUpPosition + 2;
-		int stringXPosition = borderLeftPosition + 2;
+		int stringYPosition = borderYPosition + messageBoxTopPadding;
+		int stringXPosition = borderXPosition + messageBoxLeftPadding;
 
 		g.setFont(font);
 		g.clear();
-		g.drawRect(borderLeftPosition, borderUpPosition, borderWidth,
-				borderHeight);
-		g.drawString(message, stringXPosition, stringYPosition, 1);
+		g.drawRect(borderXPosition, borderYPosition, borderWidth, borderHeight);
+		g.drawString(message, stringXPosition, stringYPosition, 0);
 
 	}
 
@@ -96,7 +95,6 @@ public class GUI {
 	 */
 	public void lightSensorAlright(SensorPosition position) {
 		if (!popUp) {
-			Font font = Font.getSmallFont();
 			g.setFont(font);
 			Image icon = Icons.ok.getIcon();
 			int imageYposition = font.getHeight() + 1;
@@ -104,18 +102,17 @@ public class GUI {
 			g.clear();
 
 			if (position == SensorPosition.Left) {
-				String message = "Left sensor";
-
-				g.drawString(message, 0, 0, 1);
-				g.drawImage(icon, 0, imageYposition, 1);
+				String message = "Left";
+				g.drawString(message, 0, 0, 0);
+				g.drawImage(icon, 0, imageYposition, 0);
 
 			} else {
-				String message = "Right Sensor";
-				int stringXPosition = 100 - message.length();
+				String message = "Right";
+				int stringXPosition = 100 - message.length()*fontPixalWidth;
 				int imageXPosition = 100 - icon.getWidth();
 
-				g.drawString(message, stringXPosition, 0, 1);
-				g.drawImage(icon, imageXPosition, imageXPosition, 1);
+				g.drawString(message, stringXPosition, 0, 0);
+				g.drawImage(icon, imageXPosition, imageYposition, 0);
 			}
 		}
 	}
@@ -128,7 +125,6 @@ public class GUI {
 	 */
 	public void lightensorError(SensorPosition position) {
 		if (!popUp) {
-			Font font = Font.getSmallFont();
 			g.setFont(font);
 			Image icon = Icons.error.getIcon();
 			int imageYposition = font.getHeight() + 1;
@@ -136,17 +132,17 @@ public class GUI {
 			g.clear();
 
 			if (position == SensorPosition.Left) {
-				String message = "Left sensor";
-				g.drawString(message, 0, 0, 1);
-				g.drawImage(icon, 0, imageYposition, 1);
+				String message = "Left";
+				g.drawString(message, 0, 0, 0);
+				g.drawImage(icon, 0, imageYposition, 0);
 
 			} else {
-				String message = "Right Sensor";
-				int stringXPosition = 100 - message.length();
+				String message = "Right";
+				int stringXPosition = 100 - message.length()*fontPixalWidth;
 				int imageXPosition = 100 - icon.getWidth();
 
-				g.drawString(message, stringXPosition, 0, 1);
-				g.drawImage(icon, imageXPosition, imageXPosition, 1);
+				g.drawString(message, stringXPosition, 0, 0);
+				g.drawImage(icon, imageXPosition, imageYposition, 0);
 			}
 		}
 	}
--- EindProject/src/nxt/Main.java
+++ EindProject/src/nxt/Main.java
@@ -31,17 +31,17 @@ public class Main {
 	 * 
 	 */
 	public static void main(String[] args) {
-		//
-		Button.waitForAnyPress();
 		MyColorSensor cs = new MyColorSensor(SensorPort.S1,
 				SensorPosition.Right);
 		MyLightSensor ls = new MyLightSensor(SensorPort.S4, SensorPosition.Left);
 		MyUltraSonicSensor us = new MyUltraSonicSensor(SensorPort.S2);
 
 		GUI gui = new GUI();
+		Button.waitForAnyPress();
 
 		new CalibrationController(cs, ls, gui);
 		new ObstructionController(cs, ls, us, gui);
 		new FollowTheLine(cs, ls);
+
 	}
 }
